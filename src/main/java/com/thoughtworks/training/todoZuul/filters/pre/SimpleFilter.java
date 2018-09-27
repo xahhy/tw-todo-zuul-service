c0ab481f35c2ad8414ab3571056e837679653fb7
hoyhe <hoyhe@thoughtworks.com>
2018-09-27 15:58:00 +0800

    Add fail-fast to assure get config from config server

--- src/main/java/com/thoughtworks/training/todoZuul/filters/pre/SimpleFilter.java
+++ src/main/java/com/thoughtworks/training/todoZuul/filters/pre/SimpleFilter.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.ZuulFilter;
 
+import com.sun.xml.internal.ws.client.ResponseContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,7 +31,6 @@ public class SimpleFilter extends ZuulFilter {
     public Object run() {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
-
         log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
 
         return null;
