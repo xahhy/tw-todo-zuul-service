6e234e8c61672e60b43244c7293fa2101eb12432
hoyhe <hoyhe@thoughtworks.com>
2018-09-29 21:54:11 +0800

    Remove consul and use docker swarm

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ COPY ./entrypoint.sh /entrypoint.sh
 ENV JAVA_OPTS=""
 ENV PROFILE="pro"
 HEALTHCHECK --interval=10s --timeout=3s \
-  CMD curl -f http://localhost:8088/health || exit 1
+  CMD curl -f http://localhost:8088/api/health || exit 1
 EXPOSE 8088
 ENTRYPOINT ["sh", "entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-while ! nc -z config-service 8888 ; do
-    echo "Waiting for upcoming Config Server"
-    sleep 2
-done
-java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=$PROFILE -jar /app.jar
+if [ -z "$1" ]; then
+    while ! nc -z config-service 8888 ; do
+        echo "Waiting for upcoming Config Server"
+        sleep 2
+    done
+    java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=$PROFILE -jar /app.jar
+fi
+
+exec "$@"
